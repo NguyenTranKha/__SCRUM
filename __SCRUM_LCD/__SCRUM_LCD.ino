db52e62e9babff225b0c83808593c191ead2e5c1
TECHMASTER <15520329@gm.uit.edu.vn>
2018-07-05 13:19:07 +0700

    done

--- __SCRUM_LCD/__SCRUM_LCD.ino
+++ __SCRUM_LCD/__SCRUM_LCD.ino
@@ -9,10 +9,16 @@ LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars
 const int DHTPIN = D0;       //Đọc dữ liệu từ DHT11 ở chân 2 trên mạch Arduino
 const int DHTTYPE = DHT11;  //Khai báo loại cảm biến, có 2 loại là DHT11 và DHT22
 DHT dht(DHTPIN, DHTTYPE);
+int gas_din=D4;
+int gas_ain=A0;
+int ad_value; 
 
 void setup()
 {
   Serial.begin(9600);
+  pinMode(gas_din,INPUT);
+  pinMode(gas_ain,INPUT);
+  
   dht.begin();         // Khởi động cảm biến
   lcd.init();                      // initialize the lcd 
   // Print a message to the LCD.
@@ -31,9 +37,12 @@ void setup()
 
 void loop()
 {
+  //arduino lcd.nobacklight
   int h = dht.readHumidity();    //Đọc độ ẩm
   int t = dht.readTemperature(); //Đọc nhiệt độ
   int f = dht.readTemperature(true);
+  ad_value=analogRead(gas_ain);
+ 
 
   if (isnan(h) || isnan(t) || isnan(f)) {
     lcd.setCursor(1,0);
@@ -41,55 +50,52 @@ void loop()
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
+  String Temperature = "Temperature:" + String(t) + "'C";
+  String Humidity = "Humidity:" + String(h) + "%";
+  String Gas = "GAS:"+String(ad_value)+"%";
+  String StateH="Normal";//////Trang thai canh bao Do Am
+  String StateT="Normal";//////Trang thai canh bao Nhiet Do
+  String StateG="CO not leak";
+      if(t < 20)
+        {
+          StateT = "Cold";//////////Luu trang thai hien tai dang xay ra cua nhiet do
+        }
+      if(t > 30)
+        {
+          StateT = "Hot";///////////Luu trang thai hien tai dang xay ra cua nhiet do
+        }
+      if(h > 85)
+        {
+          StateH = "Wet";///////////Luu trang thai hien tai dang xay ra cua do am
+        }
+      if(h < 60)
+        {
+          StateH = "Dried";////////Luu trang thai hien tai dang xay ra cua do am
+        }
+      if(digitalRead(digitalRead(gas_din) == HIGH))
+        {
+          StateG = "CO leak";
+        }
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print(Temperature);/////Hien thi lcd trang thai canh bao nhiet do
+   lcd.setCursor(0,1);
+   lcd.print(StateT);/////Hien thi lcd trang thai canh bao nhiet do
+   delay(3000);
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print(Humidity);/////Hien thi lcd trang thai canh bao nhiet do
+   lcd.setCursor(0,1);
+   lcd.print(StateH);/////Hien thi lcd trang thai canh bao nhiet do
+   delay(3000);
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print(Gas);/////Hien thi lcd trang thai canh bao nhiet do
+   lcd.setCursor(0,1);
+   lcd.print(StateG);/////Hien thi lcd trang thai canh bao nhiet do
+   delay(3000);
+   
 
-  if(t < 20 || t > 30 || h < 60 || h > 85) /////////////////////tap cac truong hop xay ra
-  {
-    String StateH="Normal:";//////Trang thai canh bao Do Am
-    String StateT="Normal:";//////Trang thai canh bao Nhiet Do
-    if(t < 20)
-    {
-      StateT = "IT'S COLD";//////////Luu trang thai hien tai dang xay ra cua nhiet do
-    }
-    if(t > 30)
-     {
-      StateT = "IT'S HOT";///////////Luu trang thai hien tai dang xay ra cua nhiet do
-    }
-    if(h > 85)
-     {
-      StateH = "IT'S WET";///////////Luu trang thai hien tai dang xay ra cua do am
-    }
-    if(h < 60)
-     {
-      StateH = "IT'S DRIED:";////////Luu trang thai hien tai dang xay ra cua do am
-    }
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print(StateT);/////Hien thi lcd trang thai canh bao nhiet do
-    lcd.setCursor(11,0);
-    lcd.print(t,DEC);/////Hien thi lcd nhiet do luc dang canh bao
-    lcd.setCursor(14,0);
-    lcd.print("'C");
-    lcd.setCursor(0,1);
-    lcd.print(StateH);//Hien thi lcd trang thai canh bao do am
-    lcd.setCursor(11,1);
-    lcd.print(h,DEC);/////Hien thi lcd nhiet do luc dang canh bao
-    lcd.setCursor(14,1);
-    lcd.print("%");
-  }else{////////////////////////xuat he so moi truong
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("T:");
-        lcd.setCursor(2,0);
-        lcd.print(t,DEC);
-        lcd.setCursor(5,0);
-        lcd.print("'C");
-        lcd.setCursor(10,0);
-        lcd.print("H:");
-        lcd.setCursor(12,0);
-        lcd.print(h,DEC);
-        lcd.setCursor(15,0);
-        lcd.print("%");
-    }
   Serial.print("Nhiet do: ");
   Serial.println(t);               //Xuất nhiệt độ
   Serial.print("Do am: ");
